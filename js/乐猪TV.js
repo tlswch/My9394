845a4fc332300f29a1982c27e6e82a371dd5b779
qizhen15800 <qizhen15800@163.com>
2022-12-14 08:18:18 +0000

    更新 'js/乐猪TV.js'

--- js/乐猪TV.js
+++ js/乐猪TV.js
@@ -1,19 +1,24 @@
-var rule = {
-    title:'乐猪TV',
+var title:'乐猪TV',
     host:'http://www.lezhutv.com',
     // homeUrl:'/',
     url:'/type/fyclass-fypage.html',
-    searchUrl:'/search-pg-fypage-wd-**.html',
+    url:'/list/fyclassfyfilter.html',
+    filterable:1,//是否启用分类筛选,
+    filter_url:'_fypage_desc_{{fl.by}}_0_0___',
+    filter: {"1":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"2":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"4":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"3":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"14":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"15":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}]},
+    // searchUrl:'/search-pg-fypage-wd-**.html',
+    searchUrl:'/index.php?m=vod-search#wd=**&search=;post',
     searchable:2,
-    quickSearch:0,
-    headers:{
-        'User-Agent':'UC_UA'
+        // 'Cookie':'test',
     },
     timeout:5000,
     class_parse:'div.nav a;a&&Text;a&&href;/(\\d.+).html',
+    // class_parse:'div.nav a;a&&Text;a&&href;/(\\d.+).html',
+    class_parse:'div.nav a;a&&Text;a&&href;/(\\d+)-1.html',
     play_parse:true,
     lazy:'',
     limit:5,
+
     推荐:'ul.tbox_m2;li;*;*;*;*',
     double:true, // 推荐内容是否双层定位
     一级:'ul.tbox_m2 li;a&&title;a&&data-original;span&&Text;a&&href',
